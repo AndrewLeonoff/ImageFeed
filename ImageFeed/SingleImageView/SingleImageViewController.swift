f114ceb86958e8d3c1ff0def95e3ae144092182f
Andrew Leonov <andrewleonov999@gmail.com>
2025-05-07 17:21:17 +0300

    Setup Single Image Screen

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Andrew Leonov on 06.05.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
